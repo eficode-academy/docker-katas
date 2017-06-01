9af25f0d2c234af5bc69513fc3c5388b904ee77e
Sofus Albertsen <soa@praqma.net>
2017-06-01 15:00:40 +0200

    made sharing in 5.md, missing part about images

--- exercises/4/app.py
+++ exercises/4/app.py
@@ -1,9 +1,11 @@
-From flask import Flask
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    return 'Hello! I am a Flask application'
 
 if __name__ == '__main__':
+    # Note the extra host argument. If we didn't have it, our Flask app
+    # would only respond to requests from inside our container
     app.run(host='0.0.0.0')
