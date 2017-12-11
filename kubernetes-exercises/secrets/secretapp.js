659f432ace5c92750dfbb5878ee7c5af0775c4fa
Mike Long <mike@praqma.com>
2017-12-11 19:08:04 +0100

    Update secretapp.js
    Use environment variables

--- kubernetes-exercises/secrets/secretapp.js
+++ kubernetes-exercises/secrets/secretapp.js
@@ -1,8 +1,8 @@
 // Licensed under the Apache License, Version 2.0 (the "License")
 var http = require('http');
 var server = http.createServer(function (request, response) {
-  const language = 'English';
-  const API_KEY = '123-456-789';
+  const language = process.env.LANGUAGE;
+  const API_KEY = process.env.API_KEY;
   response.write(`Language: ${language}\n`);
   response.write(`API Key: ${API_KEY}\n`);
   response.end(`\n`);
