29c5b9e683d60405e23e8243488958652efad5a5
Sofus Albertsen <soa@praqma.com>
2019-09-05 15:34:20 +0200

    added hostname to output of flask app getting build

--- labs/building-an-image/app.py
+++ labs/building-an-image/app.py
@@ -1,9 +1,11 @@
+import socket
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello! I am a Flask application'
+    return "Hello! I am a Flask application running on {}".format(socket.gethostname())
+
 
 if __name__ == '__main__':
     # Note the extra host argument. If we didn't have it, our Flask app
